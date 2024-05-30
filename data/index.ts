b7c22034de25b04c55906b680570c1931da6e2a0
Daniel Painter <danielpainter@Daniels-iMac.local>
2024-05-30 16:31:01 -0400

    JSON Updated

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ export const navItems = [
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
       titleClassName: "justify-end",
-      img: "/b1.svg",
+      img: "./b1.svg",
       spareImg: "",
     },
     {
@@ -43,8 +43,8 @@ export const navItems = [
       className: "lg:col-span-2 md:col-span-3 md:row-span-1",
       imgClassName: "",
       titleClassName: "justify-start",
-      img: "/grid.svg",
-      spareImg: "/b4.svg",
+      img: "./grid.svg",
+      spareImg: "./b4.svg",
     },
   
     {
@@ -54,8 +54,8 @@ export const navItems = [
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
       titleClassName: "justify-center md:justify-start lg:justify-center",
-      img: "/b5.svg",
-      spareImg: "/grid.svg",
+      img: "./b5.svg",
+      spareImg: "./grid.svg",
     },
     {
       id: 6,
@@ -76,8 +76,8 @@ export const navItems = [
       title: "SmithTrade",
       hovTitle: "Visit",
       des: "E-Commerce marketplace for electronic components",
-      img: "/p2.svg",
-      iconLists: ["/figma-icon.svg", "/tail.svg", "/google-md-icon.svg", "/angular-icon.svg"],
+      img: "./p2.svg",
+      iconLists: ["./figma-icon.svg", "./tail.svg", "./google-md-icon.svg", "./angular-icon.svg"],
       link: "https://trade.smithweb.com/",
     },
     {
@@ -85,8 +85,8 @@ export const navItems = [
       title: "Veeva Systems",
       hovTitle: "Visit",
       des: "Led project revamping navigation for company's product expansion, alongside creating standardized templates for enhanced scalability..",
-      img: "/p1.svg",
-      iconLists: ["/adobe-xd-icon.svg", "/html-icon.svg", "/css-icon.svg", "bootstrap-5-logo-icon.svg"],
+      img: "./p1.svg",
+      iconLists: ["./adobe-xd-icon.svg", "./html-icon.svg", "./css-icon.svg", "./bootstrap-5-logo-icon.svg"],
       link: "https://www.veeva.com/",
     },
     {
@@ -94,8 +94,8 @@ export const navItems = [
       title: "AI Image SaaS - Canva Application",
       hovTitle: "Visit",
       des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      img: "./p3.svg",
+      iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./c.svg"],
       link: "https://github.com/adrianhajdin/ai_saas_app",
     },
     {
@@ -104,7 +104,7 @@ export const navItems = [
       hovTitle: "Visit",
       des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
       img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      iconLists: ["./next.svg", "./tail.svg", "./ts.svg", "./three.svg", "./gsap.svg"],
       link: "https://github.com/adrianhajdin/iphone",
     },
   ];
@@ -115,21 +115,21 @@ export const navItems = [
         "Danny's expertise in UX design is evident in his work, particularly in enhancing the user experience for our suite of internal applications. Danny has great experience with both mobile and web design. His innovative approach and expert handling of tools like Figma have not only improved our design processes but also played a crucial role in the seamless transition from Sketch to Figma. This move was instrumental in rebuilding our internal design system, enhancing collaboration and efficiency across the team.",
       name: "Samuel (Sam) Hinkhouse",
       title: "Lead UX/UI Designer | Frontend Engineer ",
-      img: "/SamHinkProfile.svg"
+      img: "./SamHinkProfile.svg"
     },
     {
       quote:
         "Collaborating with Danny was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Danny's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Danny is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
-      img: "/profile.svg"
+      img: "./profile.svg"
     },
     {
       quote:
         "Collaborating with Danny was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Danny's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Danny is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
-      img: "/profile.svg"
+      img: "./profile.svg"
     },
   ];
   
@@ -137,26 +137,26 @@ export const navItems = [
     {
       id: 1,
       name: "Smith",
-      img: "/smith-logo.svg",
-      nameImg: "/smith-words.svg",
+      img: "./smith-logo.svg",
+      nameImg: "./smith-words.svg",
     },
     {
       id: 2,
       name: "Veeva",
-      img: "/veeva.svg",
-      nameImg: "/Veeva_Systems_Logo.svg",
+      img: "./veeva.svg",
+      nameImg: "./Veeva_Systems_Logo.svg",
     },
     {
       id: 3,
       name: "Value City",
-      img: "/vcf.svg",
-      nameImg: "/vc-type-logo.svg",
+      img: "./vcf.svg",
+      nameImg: "./vc-type-logo.svg",
     },
     {
       id: 4,
       name: "Victoria's Secret",
-      img: "/vs-icon-logo.svg",
-      nameImg: "/VS-logo.svg",
+      img: "./vs-icon-logo.svg",
+      nameImg: "./VS-logo.svg",
     },
     // {
     //   id: 5,
@@ -172,42 +172,42 @@ export const navItems = [
       title: "Frontend Engineer Intern",
       desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
       className: "md:col-span-2",
-      thumbnail: "/exp1.svg",
+      thumbnail: "./exp1.svg",
     },
     {
       id: 2,
       title: "Mobile App Dev - JSM Tech",
       desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
       className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp2.svg",
+      thumbnail: "./exp2.svg",
     },
     {
       id: 3,
       title: "Freelance App Dev Project",
       desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
       className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp3.svg",
+      thumbnail: "./exp3.svg",
     },
     {
       id: 4,
       title: "Lead Frontend Developer",
       desc: "Developed and maintained user-facing features using modern frontend technologies.",
       className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
+      thumbnail: "./exp4.svg",
     },
   ];
   
   export const socialMedia = [
     {
       id: 1,
-      img: "/git.svg",
+      img: "./git.svg",
     },
     {
       id: 2,
-      img: "/twit.svg",
+      img: "./twit.svg",
     },
     {
       id: 3,
-      img: "/link.svg",
+      img: "./link.svg",
     },
   ];
